ab06e03cc6f85d69d0d0becb10fc6e314f816c20
Alex Schumann <rubin@physics.umn.edu>
2013-01-23 15:35:13 -0600

    reworked to use a single plugin hook instead of peacemeal. much more reliable now. Needs a different dokuwiki patch to work, but its a 1 liner

--- action.php
+++ action.php
@@ -43,165 +43,113 @@ class action_plugin_physmagicacl extends DokuWiki_Action_Plugin {
      * plugin should use this method to register its handlers with the dokuwiki's event controller
      */
     function register(&$controller) {
-      $controller->register_hook('DOKUWIKI_STARTED','BEFORE', $this, 'handle_magicacl');
-      $controller->register_hook('MEDIAMANAGER_STARTED','BEFORE', $this, 'handle_magicacl', 'mediamanager');
-      $controller->register_hook('MEDIA_SENDFILE_PREPROCESS','BEFORE', $this, 'handle_magicacl', 'sendfile');
+      $controller->register_hook('CHECK_ACL', 'BEFORE', $this, 'handle_magicacl2');
     }
     
-    function handle_magicacl(&$event, $param) {
+    function handle_magicacl2(&$event, $param) {
       $debug = true;
       global $ID;
       global $AUTH_ACL;
       global $INFO;
       global $USERINFO;
 
+      $dodebug = false;
+      if($debug && $_GET['doit']) {
+         $dodebug = true;
+      }
+      //print "DEBUG: ". $_SERVER['REMOTE_ADDR'];
+//      if($_SERVER['REMOTE_ADDR'] == '134.84.199.3') {
+//        $dodebug = true;
+//      }
 
-      if ($debug && $_GET['doit']) {
-        print '<pre>Debugging it' . "\n";
+      $ns = $event->data['ns'];
+
+      if ($dodebug) {
+        print "<pre>\n\n";
+        print "physmagicACL pluggin hook running. ns=$ns\n";
       }
 
       $areas     = $this->getConf('areas');
 
-      if ($debug && $_GET['doit']) {
-        print 'Area Count: ' . count($areas) . "\n";
-        print 'Plugin Name: ' . $this->getPluginName() . "\n";
-      }
-
       if (!count($areas)) return;
 
-      // We need to do some specialness to get the namespace if this is the media manager
-      if ($param == 'mediamanager') {
-        // Adapted from the start of lib/exe/mediamanager.php
-        if($_REQUEST['delete']){
-            $DEL = cleanID($_REQUEST['delete']);
-            $NS  = getNS($DEL);
-        }elseif($_REQUEST['edit']){
-            $IMG = cleanID($_REQUEST['edit']);
-            $NS  = getNS($IMG);
-        }elseif($_REQUEST['img']){
-            $IMG = cleanID($_REQUEST['img']);
-            $NS  = getNS($IMG);
-        }else{
-            $NS = $_REQUEST['ns'];
-            $NS = cleanID($NS);
-        }
-        $ns = $NS;
-      }
-      elseif($param == 'sendfile') {
-        $MEDIA  = cleanID(stripctl(getID('media',false)));
-        $ns = $MEDIA;
-      } else {
-        $ns = $ID;
-      }
-
-      if ($debug && $_GET['doit']) {
-        print 'Area: ' . $ns . "\n";
-	print '$areas: '."\n";
-	print_r($areas);
-	print "\n";
-      }
-
-
       foreach ($areas as $magicns => $area) {
-        //$magicns = $area['ns_base'];
-        $level = $area['level'];
-        $cust_acl = $area['acl'];
-        //$perm = $area['perm_level'];
-        //$groupbase = $area['group_base'];
-        if($debug && $_GET['doit']) {
-            print "DEBUG: In foreach. magicns=$magicns, area=$area. level=$level, cust_acl=$cust_acl, ns=$ns<BR>\n";
-        }
-
-        if (!$cust_acl || !$level) continue;
-
-        // Only continue if we are in magic land
-        // (and magic land doesn't have any odd characters...)
-        if ((stripos($ns, $magicns) === 0) && (count(preg_grep('/^[:\-\.\w]*$/',array($ns))))) {
-          // Check for any groups that match the namespace
-          $groups = $USERINFO['grps'];
-          foreach($groups as $group) {
-            $area_slice = explode('_', $group, $level);
-            if ($debug && $_GET['doit']) {
-              print "group: $group, slices: ";
-              print_r($area_slice);
+            //$magicns = $area['ns_base'];
+            $level = $area['level'];
+            $cust_acl = $area['acl'];
+            //$perm = $area['perm_level'];
+            //$groupbase = $area['group_base'];
+            if($dodebug) {
+                print "Looking at physmagicacl config area:$magicns\n";
             }
-            if (count($area_slice) == $level && $area_slice[0] == $magicns) {
-              $namespace['ns'] = implode(':', $area_slice);
-              $namespace['group'] = $group;
-              $namespaces[] = $namespace;
-              if ($debug && $_GET['doit']) {
-                print "Adding ns " . $namespace['ns'] . " and group $group\n";
-              }
-            }
-          }
-          // Take the current ns, explode it to the specified level
-          // and implode it back to get the namespace and group for ACL
-          $magicns = array_slice(explode(':', $ns), 0, $level);
-          $rootns = implode(':', $magicns);
-          $group  = ($groupbase ? $groupbase : '') . implode('_', $magicns);
-          if ($debug && $_GET['doit']) {
-            print "group: $group, rootns: $rootns, magicns: ";
-            print_r($magicns);
-          }
 
-          // Update ACL
-
-          if ($cust_acl) {
-            foreach ($cust_acl as $entry) {
-              if (!empty($entry)) {
-                $entryarr = preg_split('/\s+/', $entry);
-                if ($debug && $_GET['doit']) {
-                  print "entryarr: ";
-                  print_r($entryarr);
+            if (!$cust_acl || !$level) {
+                if($dodebug) {
+                    print "DEBUG: bailing because no cust_acl or no level\n";
                 }
-                if (count($entryarr) >= 3) {
-                  $AUTH_ACL[] = $rootns . $entryarr[0] . ' ' . auth_nameencode(str_replace('%GROUP%', $group, $entryarr[1]), true) . ' ' . $entryarr[2];
-                  foreach ($namespaces as $aclns) {
-                    $AUTH_ACL[] = $aclns['ns'] . $entryarr[0] . ' ' . auth_nameencode(str_replace('%GROUP%', $aclns['group'], $entryarr[1]), true) . ' ' . $entryarr[2];
+                continue;
+            }
+
+            $namespaces = Array();
+            // continue only if $magicns area is a substring of $ns and theres no weird chars in $ns
+            if ( preg_match("/^$magicns/", $ns) && (count(preg_grep('/^[*:\-\.\w]*$/',array($ns))))) {
+                  // Check for any groups that match the namespace
+                  $groups = $USERINFO['grps'];
+                  foreach($groups as $group) {
+                        $area_slice = explode('_', $group, $level);
+                        if (count($area_slice) == $level && $area_slice[0] == $magicns) {
+                          $namespace['ns'] = implode(':', $area_slice);
+                          $namespace['group'] = $group;
+                          if ($dodebug) {
+                            print "Adding ns " . $namespace['ns'] . " and group $group\n";
+                          }
+                          $namespaces[] = $namespace;
+                        }
                   }
+                  // Take the current ns, explode it to the specified level
+                  // and implode it back to get the namespace and group for ACL
+                  $magicns = array_slice(explode(':', $ns), 0, $level);
+                  if($dodebug) {
+                      foreach ($magicns as $test) {
+                        if($test == '*') {
+                            print "Yep, $ns has a * in it\n";
+                        }
+                      }
+                  }
+                  $rootns = implode(':', $magicns);
+                  $group  = ($groupbase ? $groupbase : '') . implode('_', $magicns);
+
+                  // Update ACL
+
+                  if ($cust_acl) {
+                        foreach ($cust_acl as $entry) {
+                          if (!empty($entry)) {
+                            $entryarr = preg_split('/\s+/', $entry);
+                            if ($dodebug) {
+                              //print "entryarr: ";
+                              //print_r($entryarr);
+                            }
+                            if (count($entryarr) >= 3) {
+                              $AUTH_ACL[] = $rootns . $entryarr[0] . ' ' . auth_nameencode(str_replace('%GROUP%', $group, $entryarr[1]), true) . ' ' . $entryarr[2];
+                              foreach ($namespaces as $aclns) {
+                                $AUTH_ACL[] = $aclns['ns'] . $entryarr[0] . ' ' . auth_nameencode(str_replace('%GROUP%', $aclns['group'], $entryarr[1]), true) . ' ' . $entryarr[2];
+                              }
+                            }
+                          }
+                        }
+                  }
+                  // If this isn't the mediamanager, we may need to update page permisions
                 }
-              }
+            else {
+                if($dodebug) {
+                    print "Skipped area because its not in ns: $ns or ns is invalid\n";
+                }
             }
           }
-          if ($debug && $_GET['doit']) {
-            print 'Dumping: ';
-            print_r($AUTH_ACL);
-            print_r($USERINFO['grps']);
+
+          if ($dodebug) {
+            print '</pre><br>';
           }
-
-          // If this isn't the mediamanager, we may need to update page permisions
-          if ($param != 'mediamanager') {
-              // Check (possibly new) permissions
-              $new_perm = auth_quickaclcheck($ID);
-
-              if ($debug && $_GET['doit']) {
-                  print $new_perm . "OLD" . $INFO['perm'];
-              }
-
-              // Update page permissions if they are now higher.
-              // We could just do $INFO = pageinfo(), but there's a lot of overhead there
-              if ($new_perm > $INFO['perm'])
-              {
-
-                $INFO['perm'] = $new_perm;
-
-                // stolen from auth.php...
-                if($INFO['exists']){
-                  $INFO['writable'] = (is_writable($INFO['filepath']) &&
-                                      ($INFO['perm'] >= AUTH_EDIT));
-                }else{
-                  $INFO['writable'] = ($INFO['perm'] >= AUTH_CREATE);
-                }
-                $INFO['editable']  = ($INFO['writable'] && empty($INFO['lock']));
-              }
-          }
-        }
-      }
-
-      if ($debug && $_GET['doit']) {
-        print '</pre>';
-      }
-   }
+    } /* end foreach */
 }
-
 //Setup VIM: ex: et ts=4 enc=utf-8 :
